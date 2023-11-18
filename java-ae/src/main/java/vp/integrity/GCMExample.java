52bc7ba3e6153810be1f2eae47b49f460591b1a8
David Jelenc <david.jelenc@fri.uni-lj.si>
2023-11-18 21:41:41 +0100

    Almost completes assignments

--- java-ae/src/main/java/vp/integrity/GCMExample.java
+++ java-ae/src/main/java/vp/integrity/GCMExample.java
@@ -2,11 +2,9 @@ package vp.integrity;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
-import javax.crypto.Mac;
 import javax.crypto.spec.GCMParameterSpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
-import java.security.MessageDigest;
 import java.util.HexFormat;
 
 public class GCMExample {
@@ -15,30 +13,33 @@ public class GCMExample {
         final String message = "Moje sporočilo.";
         final byte[] pt = message.getBytes(StandardCharsets.UTF_8);
 
+        System.out.printf("Pošiljam: '%s'%n", new String(pt, StandardCharsets.UTF_8));
+
         // Ustvarimo naključen ključ
         final Key key = KeyGenerator.getInstance("AES").generateKey();
 
-        // Ustvarimo algoritem MAC
+        // Ustvarimo algoritem AES/GCM/NoPadding
         final Cipher ana = Cipher.getInstance("AES/GCM/NoPadding");
-        // In mu podamo ključ
-        ana.init(Cipher.ENCRYPT_MODE, key);
-        final byte[] ct = ana.doFinal(pt);
-        final byte[] iv = ana.getIV();
+        ana.init(Cipher.ENCRYPT_MODE, key); // pri tem klicu se IV samodejno izbere
+        final byte[] ct = ana.doFinal(pt); // šifriramo
+        final byte[] iv = ana.getIV(); // preberemo IV
 
-        System.out.printf("Poslala sem: '%s'%n", message);
-
-        // Šestnajstiški izpis izračunane značke
+        // Šestnajstiški izpis čistopisa, vrednosti IV in tajnopisa
         System.out.println("PT: " + HexFormat.of().formatHex(pt));
+        System.out.println("IV: " + HexFormat.of().formatHex(iv));
         System.out.println("CT: " + HexFormat.of().formatHex(ct));
 
-        // Simuliramo, da Ana pošlje par (pt, tag) Boru
+        // Prejemniku pošljemo IV in CT
+        // Odkomentirajte spodnjo vrstico, da preverite, kaj se zgodi, če se tajnopis spremeni
+        // ct[0] ^= 1;
 
-        // Bor prav tako ustvari enak algoritem MAC
+        // Prejemnik Bor prav tako ustvari enak algoritem AES/GCM/NoPadding
         final Cipher bor = Cipher.getInstance("AES/GCM/NoPadding");
-        ana.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, iv));
-        final byte[] dt = ana.doFinal(ct);
-        System.out.println("DT: " + HexFormat.of().formatHex(dt));
-        // Če se ujemata, izpišemo sporočil (a še prej ga moremo dekodirati v UTF8 niz)
+        // kot tretji parameter pri inicializaciji podamo instanco GCMParameterSpec(dolzina_značke, IV)
+        bor.init(Cipher.DECRYPT_MODE, key, new GCMParameterSpec(128, iv));
+        final byte[] dt = bor.doFinal(ct); // dešifriramo
+        System.out.println("PT: " + HexFormat.of().formatHex(dt)); // izpišemo dešifrirano vrednost
+        // Izpišemo sporočilo
         System.out.printf("Prejel sem: '%s'%n", new String(dt, StandardCharsets.UTF_8));
     }
 }
